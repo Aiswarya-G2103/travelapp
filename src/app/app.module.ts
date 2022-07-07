abbc5e4ebcac0e88459a30cafe4f6e619eda67c4
Aiswarya-G2103 <aiswaryageetha52@gmail.com>
2022-07-07 21:54:29 +0530

    searchplace completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterationComponent } from './registeration/registeration.component';
+import { SearchplaceComponent } from './searchplace/searchplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterationComponent
+    RegisterationComponent,
+    SearchplaceComponent
   ],
   imports: [
     BrowserModule,
