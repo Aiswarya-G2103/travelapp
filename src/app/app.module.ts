9ea3a094e785a4b86a7cf89b432e085647edc18f
Aiswarya-G2103 <aiswaryageetha52@gmail.com>
2022-07-07 22:42:56 +0530

    viewplace completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterationComponent } from './registeration/registeration.component';
 import { SearchplaceComponent } from './searchplace/searchplace.component';
+import { ViewplaceComponent } from './viewplace/viewplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterationComponent,
-    SearchplaceComponent
+    SearchplaceComponent,
+    ViewplaceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewplace/viewplace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewplace',
+  templateUrl: './viewplace.component.html',
+  styleUrls: ['./viewplace.component.css']
+})
+export class ViewplaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
