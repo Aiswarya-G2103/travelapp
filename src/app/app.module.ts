8f6a0addc17539d3d46925500ce4b2ff9b6acef7
Aiswarya-G2103 <aiswaryageetha52@gmail.com>
2022-07-07 21:45:22 +0530

    registeration completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterationComponent } from './registeration/registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterationComponent
   ],
   imports: [
     BrowserModule,
